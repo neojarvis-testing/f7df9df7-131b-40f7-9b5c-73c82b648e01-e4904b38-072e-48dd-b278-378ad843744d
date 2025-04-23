e62b533af2f472daf56cbce867835bc40e814bb5
bhavanakolipaka <bhavanakolipaka02@gmail.com>
2025-04-23 11:13:15 +0000

    Feedback model updated

---
+++ springapp/src/main/java/com/examly/springapp/model/Feedback.java
@@ -0,0 +1,72 @@
+package com.examly.springapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+@Entity
+public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int feedbackId;
+    String feedbackText;
+    String date;
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable=false)
+    User user;
+    @ManyToOne
+    @JoinColumn(name = "investmentId" ,nullable=true)
+    Investment investment;
+    String category;
+    public int getFeedbackId() {
+        return feedbackId;
+    }
+    public void setFeedbackId(int feedbackId) {
+        this.feedbackId = feedbackId;
+    }
+    public String getFeedbackText() {
+        return feedbackText;
+    }
+    public void setFeedbackText(String feedbackText) {
+        this.feedbackText = feedbackText;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public Investment getInvestment() {
+        return investment;
+    }
+    public void setInvestment(Investment investment) {
+        this.investment = investment;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public Feedback() {
+    }
+    public Feedback(int feedbackId, String feedbackText, String date, User user, Investment investment,
+            String category) {
+        this.feedbackId = feedbackId;
+        this.feedbackText = feedbackText;
+        this.date = date;
+        this.user = user;
+        this.investment = investment;
+        this.category = category;
+    }
+    
+
+}

2c7d995dcf9ce4cde36b32de195df5e6c8b66e78
RobinBohra <robinbohra28@gmail.com>
2025-04-28 11:19:14 +0000

    login, register commit and cors

--- springapp/src/main/java/com/examly/springapp/config/CorsConfig.java
+++ springapp/src/main/java/com/examly/springapp/config/CorsConfig.java
@@ -4,11 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
     @Bean
     public PasswordEncoder createPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        //.allowedOrigins("https://ide-cbeecfeddbeeecfdacddffaadebeddbadd.premiumproject.examly.io/proxy/8081/")
+        .allowedOrigins("*")
+        .allowedMethods("GET","POST","PUT","DELETE")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(3600);
     }
 }

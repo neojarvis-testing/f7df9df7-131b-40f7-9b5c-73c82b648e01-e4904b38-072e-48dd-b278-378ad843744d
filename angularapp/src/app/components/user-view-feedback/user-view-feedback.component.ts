5d1927c94eaa9e4c5b9a875b9ee5a1db1d75bfec
bhavanakolipaka <bhavanakolipaka02@gmail.com>
2025-04-29 16:43:03 +0000

    commit

--- angularapp/src/app/components/user-view-feedback/user-view-feedback.component.ts
+++ angularapp/src/app/components/user-view-feedback/user-view-feedback.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Feedback } from 'src/app/models/feedback.model';
+import { FeedbackService } from 'src/app/services/feedback.service';
+
 
 @Component({
   selector: 'app-user-view-feedback',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-view-feedback.component.css']
 })
 export class UserViewFeedbackComponent implements OnInit {
+  feedbacks: Feedback[] = [];
 
-  constructor() { }
+  constructor(private feedbackService: FeedbackService) { }
 
   ngOnInit(): void {
+    this.loadFeedbacks();
   }
 
+  loadFeedbacks(): void {
+    this.feedbackService.getFeedbacks().subscribe(response => {
+      this.feedbacks = response;
+    }, error => {
+      console.error('Error loading feedbacks', error);
+    });
+  }
 }

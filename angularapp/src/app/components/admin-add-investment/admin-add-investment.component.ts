20c737b0f13c8300bb532be7c93a34c3ff3dc553
ganesh3003 <motamarri.ganesh09@gmail.com>
2025-04-29 06:42:10 +0000

    admin-add-investment(a)

--- angularapp/src/app/components/admin-add-investment/admin-add-investment.component.ts
+++ angularapp/src/app/components/admin-add-investment/admin-add-investment.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-admin-add-investment',
   templateUrl: './admin-add-investment.component.html',
   styleUrls: ['./admin-add-investment.component.css']
 })
-export class AdminAddInvestmentComponent implements OnInit {
-
-  constructor() { }
-
+export class AdminAddInvestmentComponent {
   ngOnInit(): void {
   }
+  investmentForm: FormGroup;
+  showSuccessPopup = false;
+  constructor(private fb: FormBuilder) {
+    this.investmentForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      type: ['', Validators.required],
+      purcahsePrice: ['', [Validators.required, Validators.min(0)]],
+      currentPrice: ['', Validators.required],
+      quantity: ['', Validators.required],
+      purchaseDate: ['', Validators.required],
+      status: ['', Validators.required],
+    });
+  }
+  onSubmit(): void {
+    if (this.investmentForm.valid) {
+      // Normally you'd call a service here
+      this.showSuccessPopup = true;
+    }
+  }
+  closePopup(): void {
+    this.showSuccessPopup = false;
+    this.investmentForm.reset();
+  }
 
-}
+} 

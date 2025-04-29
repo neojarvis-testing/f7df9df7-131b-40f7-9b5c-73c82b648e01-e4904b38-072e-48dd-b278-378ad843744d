77ecf417b160610b5d663a851e564760ab394136
PrashanthAchanta <achantaprashanth885@gmail.com>
2025-04-29 10:50:18 +0000

    admin

--- angularapp/src/app/components/admin-view-inquiry/admin-view-inquiry.component.ts
+++ angularapp/src/app/components/admin-view-inquiry/admin-view-inquiry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InvestmentInquiry } from 'src/app/models/investment-inquiry.model';
+import { InvestmentInquiryService } from 'src/app/services/investment-inquiry.service';
 
 @Component({
   selector: 'app-admin-view-inquiry',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminViewInquiryComponent implements OnInit {
 
-  constructor() { }
+  inquiries: InvestmentInquiry[] = [];
+  filteredInquiries: InvestmentInquiry[] = [];
+  searchQuery: string = '';
+  selectedStatus: string = '';
+  selectedPriority: string = '';
+
+  constructor(private inquiryService: InvestmentInquiryService) {}
 
   ngOnInit(): void {
+    this.loadInquiries();
   }
+
+  loadInquiries(): void {
+    this.inquiryService.getAllInquries().subscribe(data => {
+      this.inquiries = data;
+      this.filteredInquiries = [...this.inquiries]; // Initialize filter results
+    });
+  }
+
+  filterInquiries(): void {
+    this.filteredInquiries = this.inquiries.filter(inquiry => {
+      return (
+        (this.selectedStatus === '' || inquiry.status === this.selectedStatus) &&
+        (this.selectedPriority === '' || inquiry.priority === this.selectedPriority) &&
+        (this.searchQuery === '' || inquiry.message.toLowerCase().includes(this.searchQuery.toLowerCase()))
+      );
+    });
+  }
+
+  deleteInquiry(inquiryId: number): void {
+    if (confirm("Are you sure you want to delete this inquiry?")) {
+      this.inquiryService.deleteInquiry(inquiryId).subscribe(() => {
+        this.loadInquiries();
+      });
+    }
+  }
+
+  addAdminResponse(inquiry: InvestmentInquiry): void {
+    const response = prompt("Enter admin response:");
+    if (response) {
+      inquiry.adminResponse = response;
+      inquiry.status = "Resolved";
+      this.inquiryService.updateInquiry(inquiry.inquiryId!, inquiry).subscribe(() => {
+        this.loadInquiries();
+      });
+    }
+  }
+
 
 }

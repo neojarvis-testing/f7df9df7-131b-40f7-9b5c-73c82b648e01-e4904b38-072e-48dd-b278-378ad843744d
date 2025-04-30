e6c5d4e7dae2d75d251a27a3568974f592cdea0a
PrashanthAchanta <achantaprashanth885@gmail.com>
2025-04-30 07:26:26 +0000

    inquiryadd

--- angularapp/src/app/components/user-view-investment/user-view-investment.component.ts
+++ angularapp/src/app/components/user-view-investment/user-view-investment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-view-investment',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserViewInvestmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  addInquiry(investmentId: number){
+    this.router.navigate(['/user/add-inquiry', investmentId]);
+  }
 }
